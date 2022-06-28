5397dc30597b936391552edf52db651a2241c017
Alessio Bonaldo <alessiobonaldo1995@gmail.com>
2022-06-28 15:17:09 +0200

    update

--- index.ts
+++ index.ts
@@ -2,3 +2,4 @@ export type { INode } from './dist/finder.types';
 export type { HeaderBehavior } from './dist//header.types';
 export { Header } from './dist/header';
 export { Finder } from './dist/finder';
+export { CloseIconBootstrap, MaxiIconBootstrap, MinIconBootstrap } from './dist/theme/styled';
